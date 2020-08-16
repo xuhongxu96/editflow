fef7b1af0d4df5adb03f62d45eed47a12edc0abc
Hongxu Xu <xuhongxu96@hotmail.com>
2020-08-16 16:31:46 +0800

    Fix box selection when scale changes.

--- src/hooks/flow/useBoxSelection.ts
+++ src/hooks/flow/useBoxSelection.ts
@@ -6,20 +6,20 @@ import { useEventListener } from 'hooks';
 import { isIntersected } from 'utils';
 
 export const useBoxSelection = () => {
-  const { nodeIdQuadTree, clientRect, viewBound, raw } = useFlowContext();
+  const { scale, nodeIdQuadTree, clientRect, viewBound, raw } = useFlowContext();
   const dispatch = useFlowDispatchContext();
   const [selection, setSelection] = useState<IRect>();
 
   const [startSelection, _stopSelection, onSelection] = useMoving(
     useCallback(
       (offset, initPos) => {
-        initPos.x += -clientRect.x + viewBound.x;
-        initPos.y += -clientRect.y + viewBound.y;
+        initPos.x += -clientRect.x;
+        initPos.y += -clientRect.y;
         const rect = {
-          x: offset.x < 0 ? initPos.x + offset.x : initPos.x,
-          y: offset.y < 0 ? initPos.y + offset.y : initPos.y,
-          w: Math.abs(offset.x),
-          h: Math.abs(offset.y),
+          x: (offset.x < 0 ? initPos.x + offset.x : initPos.x) / scale + viewBound.x,
+          y: (offset.y < 0 ? initPos.y + offset.y : initPos.y) / scale + viewBound.y,
+          w: Math.abs(offset.x) / scale,
+          h: Math.abs(offset.y) / scale,
         };
         setSelection(rect);
         const nodeIds = nodeIdQuadTree
@@ -27,7 +27,7 @@ export const useBoxSelection = () => {
           .filter(id => isIntersected(rect, raw.nodes[id].layout));
         dispatch({ type: 'setSelectNodes', ids: nodeIds });
       },
-      [clientRect, nodeIdQuadTree, viewBound, dispatch, raw]
+      [clientRect, scale, nodeIdQuadTree, viewBound, dispatch, raw]
     )
   );
 
